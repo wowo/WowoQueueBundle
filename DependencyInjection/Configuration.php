f8468b5f92ea9ad13800231896e1c96c8f7426a3
Wojciech Sznapka <wojciech.sznapka@xsolve.pl>
2013-10-10 10:59:28 +0200

    Fixed violations reported by SensioLabsInsight

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('wowo_queue');
+        $treeBuilder->root('wowo_queue');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
